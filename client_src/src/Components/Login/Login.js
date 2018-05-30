03a5da49e5b8647413b662df8768a23149ff04b0
Reham <reham.abdelraheem95@gmail.com>
2018-05-30 15:40:30 +0200

    Semantic UI React included, Login Form Added.

---
+++ client_src/src/Components/Login/Login.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { Button, Form, Grid, Header, Segment } from 'semantic-ui-react'
+import './Login.css'
+
+class Login extends Component {
+    handleClick() {
+        console.log('clicked')
+    }
+    render() {
+        return (
+            <div className='login-form'>
+                <style></style>
+                <Grid
+                    textAlign='center'
+                    style={{ height: '100%' }}
+                    verticalAlign='middle'
+                >
+                    <Grid.Column style={{ maxWidth: 450 }}>
+                        <Header as='h2' color='blue' textAlign='center'>
+                            {' '}Log-in to your account
+                        </Header>
+                        <Form size='large'>
+                            <Segment stacked>
+                                <Form.Input
+                                    fluid
+                                    icon='user'
+                                    iconPosition='left'
+                                    placeholder='E-mail address'
+                                />
+                                <Form.Input
+                                    fluid
+                                    icon='lock'
+                                    iconPosition='left'
+                                    placeholder='Password'
+                                    type='password'
+                                />
+
+                                <Button color='teal' fluid size='large'>Login</Button>
+                            </Segment>
+                        </Form>
+                    </Grid.Column>
+                </Grid>
+            </div>
+        )
+    }
+}
+
+export default Login;
