6e30ebb6697bf87f9b55e165582ce9e7ca704365
Reham <reham.abdelraheem95@gmail.com>
2018-06-06 16:20:06 +0200

    Expansion, Main Components Added.

--- client-src/src/app/app.module.ts
+++ client-src/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+import { ExpansionComponent } from './components/main/expansion/expansion.component';
 
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -17,6 +19,8 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     LoginComponent,
+    MainComponent,
+    ExpansionComponent,
   ],
   imports: [
     BrowserModule,
