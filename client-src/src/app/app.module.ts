a1b571633adc44237183b542b92f11b4d757004e
Reham <reham.abdelraheem95@gmail.com>
2018-06-06 20:14:54 +0200

    Main Components Updated.

--- client-src/src/app/app.module.ts
+++ client-src/src/app/app.module.ts
@@ -5,14 +5,21 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
+import { BranchesComponent } from './components/main/branches/branches.component';
 import { ExpansionComponent } from './components/main/expansion/expansion.component';
+import { CustomerComponent } from './components/main/customer/customer.component'
 
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '',
     redirectTo: '/login',
     pathMatch: 'full'
-  }
+  },
+  { path: 'main', component: MainComponent },
+  { path: 'main', component: CustomerComponent },
+  { path: 'main', component: BranchesComponent },
+  { path: 'main', component: MainComponent },
+  { path: 'main', component: MainComponent },
 ];
 
 @NgModule({
@@ -21,6 +28,8 @@ const appRoutes: Routes = [
     LoginComponent,
     MainComponent,
     ExpansionComponent,
+    CustomerComponent,
+    BranchesComponent
   ],
   imports: [
     BrowserModule,
