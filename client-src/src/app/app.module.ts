d42ede6854982d0561d4291a0f71aff1ff023608
Reham <reham.abdelraheem95@gmail.com>
2018-06-06 15:50:41 +0200

    Routing for Login Page.

--- client-src/src/app/app.module.ts
+++ client-src/src/app/app.module.ts
@@ -3,6 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
@@ -10,7 +19,11 @@ import { LoginComponent } from './components/login/login.component';
     LoginComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
