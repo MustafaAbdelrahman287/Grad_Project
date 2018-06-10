b777768933025d5a9cc91284687e5bc1e84bd14e
Reham <reham.abdelraheem95@gmail.com>
2018-06-10 23:41:52 +0200

    Adding geojson object to the map (not working!).

--- client-src/src/app/components/main/map/map.component.ts
+++ client-src/src/app/components/main/map/map.component.ts
@@ -9,10 +9,11 @@ import { Http } from '@angular/http';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-  geojsonFeature: any;
+  mymap:any;
+  geojsonLayer: any;
   constructor(http: Http) {
-    http.get('../../../../assets/map.geojson').subscribe(response => {
-    this.geojsonFeature = response.json(); console.log(this.geojsonFeature)
+    http.get('../../../../assets/STREET.json').subscribe(response => {
+    this.geojsonLayer = response.json(); console.log(this.geojsonLayer)
     });
   }
   onMapClick(event) {
@@ -20,20 +21,12 @@ export class MapComponent implements OnInit {
     console.log(l.lat, l.lng);
   }
 
-  geojsonFeature1 = {
-    "type": "Feature",
-    "geometry": {
-      "type": "Point",
-      "coordinates": [-104.99404, 39.75621]
-    }
-  };
-
   ngOnInit() {
     const mymap = L.map('mapid').setView([51.505, -0.09], 13);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?', {
       maxZoom: 18,
     }).addTo(mymap);
-    L.geoJSON(this.geojsonFeature).addTo(mymap);
+    L.geoJSON(this.geojsonLayer).addTo(mymap);
     mymap.on('click', this.onMapClick);
   }
 
