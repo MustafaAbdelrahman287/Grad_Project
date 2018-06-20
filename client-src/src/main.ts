4fb57393cfb2fc51559fb36ffabe13497db68c4a
Reham <reham.abdelraheem95@gmail.com>
2018-06-20 16:41:19 +0200

    Components editing, Styling for mobile version.

--- client-src/src/main.ts
+++ client-src/src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import * as $ from 'jquery';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -7,6 +8,11 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
+$(function () {
+  'use strict'
+  $('[data-toggle="offcanvas"]').on('click', function () {
+    $('.offcanvas-collapse').toggleClass('open')
+  })
+})
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
