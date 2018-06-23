c8aaf67d24e1779afb1f99cdfb3d6254c4a60f50
MohamedAbdelaty1994 <39759681+MohamedAbdelaty1994@users.noreply.github.com>
2018-06-24 01:51:47 +0200

    OverLap Branches

--- client-src/src/app/components/main/branches/branches.component.ts
+++ client-src/src/app/components/main/branches/branches.component.ts
@@ -3,8 +3,8 @@ import * as L from 'leaflet';
 import * as turf from '@turf/turf';
 import { BranchService } from '../../../services/branch/branch.service';
 import { CompetitorService } from '../../../services/competitor/competitor.service';
-import {IBranch} from '../../../interfaces/branch';
-import {ICompetitor} from '../../../interfaces/competitor';
+import { IBranch } from '../../../interfaces/branch';
+import { ICompetitor } from '../../../interfaces/competitor';
 
 @Component({
   selector: 'app-branches',
@@ -13,10 +13,10 @@ import {ICompetitor} from '../../../interfaces/competitor';
 })
 export class BranchesComponent implements OnInit {
   mymap: any;
-  
-  public branches=[];
-  public competitor=[];
-  constructor( private _branchService: BranchService,private _competitorService:CompetitorService) { 
+
+  public branches = [];
+  public competitor = [];
+  constructor(private _branchService: BranchService, private _competitorService: CompetitorService) {
     function createCustomIcon(latlng) {
       let myIcon = L.icon({
         iconUrl: '../../assets/adidas_PNG22.png',
@@ -27,50 +27,52 @@ export class BranchesComponent implements OnInit {
       return L.marker(latlng, { icon: myIcon, draggable: true })
     }
   }
-overlapareas= [{id:'1', name:'Adidas'}, {id:'2', name:'Competitor Branches'}];
-myIcon(url){return L.icon({
-  iconUrl: url,
-  iconRetinaUrl: url,
-  iconSize: [50, 50],
-  iconAnchor: [24, 24],
-  popupAnchor: [0, 0]
-})};
- onMapClick(event) {
-  let l = event.latlng;
-  console.log(l);
-  L.marker(l, { icon: this.myIcon('../../assets/adidas_PNG22.png'), draggable: true }).addTo(this.mymap);
-  return l;
-}
-onClick(event) {
-  this._branchService.getBranches().subscribe(
-    data => {
-      this.branches = data;
-      console.log(data);
-      for (let i = 0; i < data.length; i++) {
-        L.marker([this.branches[i].branch_location.lat, this.branches[i].branch_location.lng], { icon: this.myIcon('../../assets/adidas_PNG22.png'), draggable: true }).addTo(this.mymap).bindPopup(`Name : ${this.branches[i].name}`).addEventListener('click', this.onClick);
-      }
-    },
-    err => console.log(err)
-  )
-}
-
-onClick1(event){
-  this._competitorService.getCompetitors().subscribe(
-    data => {
-      this.competitor = data;
-      console.log(data);
-      for (let i = 0; i < data.length; i++) {
-        for (let j = 0; j <this.competitor[i].competitor_location.length; j++){ 
-           L.marker([this.competitor[i].competitor_location[j].lat, this.competitor[i].competitor_location[j].lng], { icon: this.myIcon('../../assets/clogo.png'), draggable: true }).addTo(this.mymap).bindPopup(`Name : ${this.competitor[i].name}`).addEventListener('click', this.onClick1);
-          }
-        console.log(this.competitor[i].competitor_location)    
-      }
-    },
+  overlapareas = [{ id: '1', name: 'Adidas' }, { id: '2', name: 'Competitor Branches' }];
+  myIcon(url) {
+    return L.icon({
+      iconUrl: url,
+      iconRetinaUrl: url,
+      iconSize: [50, 50],
+      iconAnchor: [24, 24],
+      popupAnchor: [0, 0]
+    })
+  };
+  onMapClick(event) {
+    let l = event.latlng;
+    console.log(l);
+    L.marker(l, { icon: this.myIcon('../../assets/adidas_PNG22.png'), draggable: true }).addTo(this.mymap);
+    return l;
+  }
+  onClick(event) {
+    this._branchService.getBranches().subscribe(
+      data => {
+        this.branches = data;
+        console.log(data);
+        for (let i = 0; i < data.length; i++) {
+          L.marker([this.branches[i].branch_location.lat, this.branches[i].branch_location.lng], { icon: this.myIcon('../../assets/adidas_PNG22.png'), draggable: true }).addTo(this.mymap).bindPopup(`Name : ${this.branches[i].name}`).addEventListener('click', this.onClick);
+        }
+      },
       err => console.log(err)
-    )  
-}
+    )
+  }
 
-/******************************************* Overlap Areas*******************************************/
+  onClick1(event) {
+    this._competitorService.getCompetitors().subscribe(
+      data => {
+        this.competitor = data;
+        console.log(data);
+        for (let i = 0; i < data.length; i++) {
+          for (let j = 0; j < this.competitor[i].competitor_location.length; j++) {
+            L.marker([this.competitor[i].competitor_location[j].lat, this.competitor[i].competitor_location[j].lng], { icon: this.myIcon('../../assets/clogo.png'), draggable: true }).addTo(this.mymap).bindPopup(`Name : ${this.competitor[i].name}`).addEventListener('click', this.onClick1);
+          }
+          console.log(this.competitor[i].competitor_location)
+        }
+      },
+      err => console.log(err)
+    )
+  }
+
+  /******************************************* Overlap Areas*******************************************/
   onClick2(event) {
     /******************************************* Branches*******************************************/
     this._branchService.getBranches().subscribe(
@@ -79,14 +81,40 @@ onClick1(event){
         let buffered;
         let buffered_coords;
         let buffered_polygon;
+        let arrayOfPolygons = [];
         this.branches = data;
         console.log(data);
         for (let i = 0; i < data.length; i++) {
           point = turf.point([data[i].branch_location.lat, data[i].branch_location.lng]);
           buffered = turf.buffer(point, 1, { units: 'miles' });
+          arrayOfPolygons.push(buffered);
           buffered_coords = turf.getCoords(buffered);
           buffered_polygon = L.polygon(buffered_coords, { color: 'gray' }).addTo(this.mymap);
           L.marker([this.branches[i].branch_location.lat, this.branches[i].branch_location.lng], { icon: this.myIcon('../../assets/adidas_PNG22.png'), draggable: true }).addTo(this.mymap).bindPopup(`Name : ${this.branches[i].name}`).addEventListener('click', this.onClick);
+        }
+        //intersection
+        console.log('arrayOfPolygons:', arrayOfPolygons);
+        let parcel;
+        let parce2;
+        let conflict;
+        let conflictlist = [];
+        let intersectionCoords;
+        let polygonOfIntersection;
+        for (let i = 0; i < arrayOfPolygons.length; i++) {
+          parcel = arrayOfPolygons[i];
+          for (let j = 0; j < arrayOfPolygons.length; j++) {
+            parce2 = arrayOfPolygons[j];
+            console.log("processing: ", i, " , ", j);
+            conflict = turf.intersect(parcel, parce2);
+            if (conflict !== null && i !== j) {
+              conflictlist.push(conflict);
+            }
+          }
+        }
+        for (let i = 0; i < conflictlist.length; i++) {
+          console.log("conflictlist: ", conflictlist);
+          intersectionCoords = turf.getCoords(conflictlist[i]);
+          polygonOfIntersection = L.polygon(intersectionCoords, { color: 'red' }).addTo(this.mymap);
         }
       },
       err => console.log(err)
@@ -95,14 +123,14 @@ onClick1(event){
   }
   /*****************************************************************************************************/
   ngOnInit() {
-    
+
     this.mymap = L.map('mapid').setView([30.09219, 31.32297], 12);
     console.log(this.mymap)
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?', {
       maxZoom: 18,
     }).addTo(this.mymap);
 
-   
+
   }
 
 }
