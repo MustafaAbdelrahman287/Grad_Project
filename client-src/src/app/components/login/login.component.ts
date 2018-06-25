66b71e79e13850c1de32ea5f48002cac632f954f
SamarMuhamed <samareid12@outlook.com>
2018-06-25 09:11:58 +0200

    Editt

--- client-src/src/app/components/login/login.component.ts
+++ client-src/src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../services/auth/auth.service';
+
 import { Router } from "@angular/router";
+
 
 @Component({
   selector: 'app-login',
@@ -9,23 +10,17 @@ import { Router } from "@angular/router";
 })
 export class LoginComponent implements OnInit {
   invalidLogin:boolean;
-  constructor(private router: Router, private authService: AuthService) { }
+  constructor(private router: Router) { }
   onSubmit({ value, valid }) {
     console.log(value, valid);
     if (valid) {
-      let result = this.authService.login(value);
-      console.log(result)
-      result.subscribe(result => { 
-        if (result)
-          this.router.navigate(['/main']);
-        else  
+     this.router.navigate(['/main']);}
+      else  
           this.invalidLogin = true; 
-      }, (err:Response) => {console.log(err)});
-    } else {
-      this.invalidLogin = true;
-      console.log(this.invalidLogin);
-    }
-  }
+      }
+    
+  
+  
 
 
   ngOnInit() { }
